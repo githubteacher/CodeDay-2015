7d75f972a21614d313e41760d057cf94ceccdaab
nirm123 <nirmalarunprakash@gmail.com>
2015-05-23 18:48:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 console.log("This code is sloppy");
 console.log("hello world.");
+// Why are you ignoring my comments
 
-
-while (1) {
+while (1) 
+{
+  // This endless loop is pointless
   console.log("This is fun.");
 }
