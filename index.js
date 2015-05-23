d9c1899de75ff4e3e39eb59325f633faaa4e2b65
Rishi Masand <darthanakin44@gmail.com>
2015-05-23 18:52:46 -0400

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-console.log("This code is sloppy");
-console.log("hello world.");
-// Why are you ignoring my comments
-
-while (1) 
-{
-  // This endless loop is pointless
-  console.log("This is fun.");
-}
